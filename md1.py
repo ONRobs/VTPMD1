e54a3ca76adc4333449a6d0554886e8600f59896
ONRobs <75011517+ONRobs@users.noreply.github.com>
2026-03-28 07:57:07 +0200

    Add morphological processing in md1.py

---
+++ md1.py
@@ -0,0 +1,22 @@
+import hfst
+
+generator = hfst.compile_lexc_file('md1.lexc')
+
+InsertJ = hfst.regex('[..] -> j || [b | f | m | p | v] _ "^" M')
+
+ZNReplacement = hfst.regex('z n -> ž ņ ||  _ "^" M')
+DReplacement = hfst.regex('d -> ž ||  _ "^" M')
+
+MCleanup = hfst.regex('M -> 0')
+Cleanup = hfst.regex('"^" -> 0')
+
+cascade = hfst.compose((generator, InsertJ, ZNReplacement, DReplacement, MCleanup, Cleanup))
+
+for noun in ["med", "al", "liet", "up", "zvaigzn", "cepur", "nakt", "aus", "ac", "ļaud"]:
+    for number in ["Sg", "Pl"]:
+        for form in ["Nom", "Gen", "Dat", "Acc", "Loc"]:
+            result = cascade.lookup(noun + "+N+" + number + "+" + form)
+            if result:
+                print(noun + "+N+" + number + "+" + form + " - " + result[0][0].replace("@_EPSILON_SYMBOL_@", ""))
+            else:
+                print(noun + "+N+" + number + "+" + form + " - ")
